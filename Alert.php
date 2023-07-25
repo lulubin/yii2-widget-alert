a016b7fb35c8d3ce054b9441a8673e2bf7381294
lulubin <452936616@qq.com>
2023-07-25 13:31:53 +0800

    init

---
+++ Alert.php
@@ -0,0 +1,37 @@
+<?php
+namespace lulubin\alert;
+
+class Alert extends \yii\bootstrap\Widget
+{
+    public $closeButton = [];
+    public $alertTypes = [
+        'error' => 'alert-danger',
+        'danger' => 'alert-danger',
+        'success' => 'alert-success',
+        'info' => 'alert-info',
+        'warning' => 'alert-warning'
+    ];
+
+    public function init()
+    {
+        parent::init();
+        $session = \Yii::$app->getSession();
+        $flashes = $session->getAllFlashes();
+        $appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
+        foreach ($flashes as $type => $data) {
+            if (isset($this->alertTypes[$type])) {
+                $data = (array)$data;
+                foreach ($data as $i => $message) {
+                    $this->options['class'] = $this->alertTypes[$type] . $appendCss;
+                    $this->options['id'] = $this->getId() . '-' . $type . '-' . $i;
+                    echo \yii\bootstrap\Alert::widget([
+                        'body' => $message,
+                        'closeButton' => $this->closeButton,
+                        'options' => $this->options,
+                    ]);
+                }
+                $session->removeFlash($type);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# yii2-widget-alert
-An Yii2 widget for alert after create or update successfully
+## 1.Install
+composer require --prefer-dist lulubin/yii2-widget-alert "dev-master"
+
+## 2.Usage
+Add the following code to main.php
+```html
+<div class="container">
+    <?=lulubin\alert\Alert::widget()?>
+    <?=$content ?>
+</div>
+```
